a732d97fba2c50d7bb661421ed3c458093ddaad1
Octavio Gonzalez <Octavio.gonzalez1143@gmail.com>
2021-01-30 15:57:48 -0800

    Initial Commit

---
+++ Yelpy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yelpy
+//
+//  Created by Octavio Gonzalez on 1/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
